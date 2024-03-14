082a20985e42307878df41ed4dd256f50adadf36
Tiago Mota <tiago.cmota@hotmail.com>
2024-03-14 14:42:53 +0000

    First commit

---
+++ server/index.js
@@ -0,0 +1,11 @@
+const express = require("express");
+
+const app = express();
+
+const db = require("./models");
+
+db.sequelize.sync().then(() => {
+  app.listen(3001, () => {
+    console.log("Server está a correr na porta 3001");
+  });
+});
