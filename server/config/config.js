66f98ef68a392ac5ae3ce910f59455cb66feeaa9
Tiago Mota <tiago.cmota@hotmail.com>
2024-05-26 18:15:26 +0100

    last

--- server/config/config.js
+++ server/config/config.js
@@ -1,3 +1,6 @@
+require('dotenv').config();
+
+module.exports =
 {
   "development": {
     "username": "root",
@@ -14,10 +17,10 @@
     "dialect": "mysql"
   },
   "production": {
-    "username": "root",
-    "password": null,
-    "database": "database_production",
-    "host": "127.0.0.1",
+    "username": process.env.DB_USERNAME,
+    "password": process.env.DB_PASSWORD,
+    "database": process.env.DB_DBNAME,
+    "host": process.env.DB_HOST,
     "dialect": "mysql"
   }
 }
--- server/models/index.js
+++ server/models/index.js
@@ -6,15 +6,15 @@ const Sequelize = require('sequelize');
 const process = require('process');
 const basename = path.basename(__filename);
 const env = process.env.NODE_ENV || 'development';
-const config = require(__dirname + '/../config/config.json')[env];
+const config = require(__dirname + '/../config/config.js')[env];
 const db = {};
 
 let sequelize;
 if (env === 'production') {
   // Configurações para produção
   sequelize = new Sequelize(
-    process.env.DB_NAME,
-    process.env.DB_USER,
+    process.env.DB_DBNAME,
+    process.env.DB_USERNAME,
     process.env.DB_PASSWORD,
     {
       host: process.env.DB_HOST,
@@ -22,8 +22,7 @@ if (env === 'production') {
     }
   );
 } else {
-  // Configurações para desenvolvimento
-  const config = require(__dirname + '/../config/config.json')[env];
+  // Configurações para desenvolvimento e teste
   if (config.use_env_variable) {
     sequelize = new Sequelize(process.env[config.use_env_variable], config);
   } else {
