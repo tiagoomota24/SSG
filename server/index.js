f6aaf85ab3ab6c45eea9ce1ff6aeee479311866f
Tiago Mota <tiago.cmota@hotmail.com>
2024-05-27 14:19:02 +0100

    Try_url

--- server/index.js
+++ server/index.js
@@ -22,6 +22,14 @@ app.use('/contact', contactRoutes);
 const phishingRouter = require("./routes/Phishing");
 app.use("/phishing", phishingRouter);
 
+// Serve static files from the React app
+app.use(express.static(path.join(__dirname, 'client/build')));
+
+// The "catchall" handler: for any request that doesn't
+// match one above, send back React's index.html file.
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
+});
 
 db.sequelize.sync().then(() => {
   app.listen(process.env.PORT || 3001, () => {
